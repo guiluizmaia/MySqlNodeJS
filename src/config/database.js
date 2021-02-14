c48dfd6f60a93a4d4831bee956ff71abb1d43975
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-02-14 17:44:14 -0300

    Add files via upload

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,11 @@
- module.exports
+ module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: '4527',
+    database: 'nodejs',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+ };
